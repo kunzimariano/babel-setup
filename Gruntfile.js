364774ff39f89c5e4832a4d2ef847d4bb523a32e
kunzimariano <kunzi.mariano@gmail.com>
2016-01-20 23:01:46 -0300

    Changed Gruntfile so it only runs babel on the files that changed.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,21 @@
 module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
+  var es6Files = {
+    expand: true,
+    cwd: 'src',
+    src: ['**/*.js'],
+    dest: 'lib',
+    ext: '.js'
+  };
+
   grunt.initConfig({
     babel: {
       options: {
         babelrc: true
       },
       dist: {
-        files: [{
-          expand: true,
-          cwd: 'src',
-          src: ['**/*.js'],
-          dest: 'lib',
-          ext: '.js'
-        }]
+        files: [es6Files]
       },
     },
     watch: {
@@ -28,4 +30,10 @@ module.exports = function(grunt) {
   grunt.registerTask('default', ['babel']);
 
   grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.event.on('watch', function(action, filepath) {
+    es6Files.src = [filepath];
+    es6Files.cwd = '';
+    grunt.config('babel.dist.files', [es6Files]);
+  });
 };
